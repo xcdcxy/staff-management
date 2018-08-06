17ab4433ebb7f7fb26ac669edc1f84f5a389a1f9
李孟浩 <472794030@qq.com>
2018-08-06 23:29:11 +0800

    完成登录验证程序
    如题

--- sm/sm/src/main/java/com/imooc/sm/controller/SelfController.java
+++ sm/sm/src/main/java/com/imooc/sm/controller/SelfController.java
@@ -5,14 +5,23 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.imooc.sm.dao.SelfDao;
+import com.imooc.sm.entity.Staff;
+import com.imooc.sm.service.SelfService;
 
 /*
  * 普通控制器
  */
 @Controller("selfController")
 public class SelfController {
+	
+	@Autowired
+	private SelfService selfService;
 	
 	/*
 	 * 登录
@@ -21,4 +30,29 @@ public class SelfController {
 		// 跳转至登录界面
 		request.getRequestDispatcher("login.jsp").forward(request, response);
 	} 
+	
+	/*
+	 * 登录验证程序
+	 */
+	public void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 获取账号和密码
+		String account = request.getParameter("account"); 
+		String password = request.getParameter("password"); 
+		// 查询用户
+		Staff staff = selfService.login(account, password);
+		if(staff == null) {
+			// 用户名或密码错误,重定向至登录页面
+			response.sendRedirect(request.getContextPath() + "/toLogin.do");
+		} else {
+			// 登录验证通过
+			HttpSession session = request.getSession();
+			session.setAttribute("USER", staff);
+			response.sendRedirect(request.getContextPath() + "/main.do");
+		}
+	} 
+	
+	// 跳转主页面
+	 public void main(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	        request.getRequestDispatcher("index.jsp").forward(request,response);
+	    }
 }
---
+++ sm/sm/src/main/java/com/imooc/sm/service/impl/SelfServiceImpl.java
@@ -0,0 +1,31 @@
+package com.imooc.sm.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.imooc.sm.dao.SelfDao;
+import com.imooc.sm.entity.Staff;
+import com.imooc.sm.service.SelfService;
+
+@Service("selfService")
+public class SelfServiceImpl implements SelfService {
+	
+	@Autowired
+	private SelfDao selfDao;
+	
+	public Staff login(String account, String password) {
+		Staff staff = selfDao.selectByAccount(account);
+		// 未查询到用户
+		if(staff == null) return null;
+		// 用户名和密码正确
+		if(staff.getPassword().equals(password))
+			return staff;
+		// 密码错误
+		return null;
+	}
+
+	public void changePassword(Integer id, String password) {
+
+	}
+
+}
