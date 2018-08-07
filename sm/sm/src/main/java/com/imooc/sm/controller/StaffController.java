3a3fbabd521732b0b0b12ce4f0a4c54251861277
李孟浩 <472794030@qq.com>
2018-08-07 17:47:07 +0800

    完成删除员工信息功能
    如题

---
+++ sm/sm/src/main/java/com/imooc/sm/controller/StaffController.java
@@ -0,0 +1,55 @@
+package com.imooc.sm.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.imooc.sm.entity.Staff;
+import com.imooc.sm.service.StaffService;
+
+@Controller("staffController")
+public class StaffController {
+	
+	@Autowired
+	private StaffService staffService;
+
+	/*
+	 * 展示所有员工信息
+	 */
+	public void list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 获取所有员工信息
+		List<Staff> list = staffService.getAll();
+		request.setAttribute("LIST", list);
+		request.getRequestDispatcher("../staff_list.jsp").forward(request, response);
+	}
+	
+	/*
+	 * 删除员工信息
+	 */
+	public void remove(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 员工编号
+		Integer id = Integer.parseInt(request.getParameter("id"));
+		// 删除员工
+		staffService.remove(id);
+		response.sendRedirect("list.do");
+	}	
+	
+	/*
+	 * 查看员工信息
+	 */
+	public void remove(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 员工编号
+		Integer id = Integer.parseInt(request.getParameter("id"));
+		// 删除员工
+		staffService.remove(id);
+		response.sendRedirect("list.do");
+	}	
+	
+	
+}
---
+++ sm/sm/src/main/java/com/imooc/sm/service/impl/StaffServiceImpl.java
@@ -0,0 +1,46 @@
+package com.imooc.sm.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.imooc.sm.dao.StaffDao;
+import com.imooc.sm.entity.Staff;
+import com.imooc.sm.service.StaffService;
+
+@Service("staffService")
+public class StaffServiceImpl implements StaffService {
+
+	@Autowired
+	private StaffDao staffDao;
+	
+	public void add(Staff staff) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void remove(Integer id) {
+		// 删除员工信息
+		staffDao.delete(id);
+	}
+
+	public void edit(Staff staff) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Staff get(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * 获取所有员工信息
+	 */
+	public List<Staff> getAll() {
+		List<Staff> list = staffDao.selectAll();
+		return list;
+	}
+
+}
