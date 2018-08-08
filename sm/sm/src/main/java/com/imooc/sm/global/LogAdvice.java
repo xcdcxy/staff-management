155696939494b7ded83b15a906228180c496cfbe
李孟浩 <472794030@qq.com>
2018-08-08 23:23:15 +0800

    完成AOP功能
    修复Bug

--- sm/sm/src/main/java/com/imooc/sm/global/LogAdvice.java
+++ sm/sm/src/main/java/com/imooc/sm/global/LogAdvice.java
@@ -3,12 +3,17 @@ package com.imooc.sm.global;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.aopalliance.intercept.Joinpoint;
 import org.apache.catalina.tribes.util.Logs;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.imooc.sm.entity.Log;
 import com.imooc.sm.entity.Staff;
@@ -17,6 +22,7 @@ import com.imooc.sm.service.LogService;
 /*
  * 日志信息代理类
  */
+@Component("logAdvice")
 @Aspect
 public class LogAdvice {
 	
@@ -26,7 +32,7 @@ public class LogAdvice {
 	/*
 	 * 操作日志
 	 */
-	@AfterReturning("execution(* com.imooc.sm.controller.*.*(..) && !execution(* com.imooc.sm.controller.*.to*(..) && !execution(* com.imooc.sm.controller.SelfController.*.*(..))")
+	@AfterReturning("execution(* com.imooc.sm.controller.*.*(..)) && !execution(* com.imooc.sm.controller.*.to*(..)) && !execution(* com.imooc.sm.controller.SelfController.*.*(..))")
 	public void operationLog(JoinPoint joinPoint) {
 		Log log = new Log();
 		log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
@@ -42,8 +48,8 @@ public class LogAdvice {
 	/*
 	 * 系统日志
 	 */
-	@AfterThrowing(throwing="e",pointcut="execution(* com.imooc.sm.controller.*.*(..) && !execution(* com.imooc.sm.controller.SelfController.*(..))")
-	public void systemLog(Throwable e, JoinPoint joinPoint) {
+	@AfterThrowing(throwing="e",pointcut="execution(* com.imooc.sm.controller.*.*(..)) && !execution(* com.imooc.sm.controller.SelfController.*(..))")
+	public void systemLog(JoinPoint joinPoint, Throwable e) {
 		Log log = new Log();
 		log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
 		log.setOperation(joinPoint.getSignature().getName());
@@ -53,6 +59,40 @@ public class LogAdvice {
 		log.setOperator(staff.getAccount());
 		log.setResult(e.getClass().getSimpleName());
 		logService.addSystemLog(log);
+	}
+	
+	/*	
+	 * 登录日志
+	 */
+	@After("execution(* com.imooc.sm.controller.SelfController.login(..))")
+	public void loginLog(JoinPoint joinPoint) {
+		System.out.println("login...");
+		log(joinPoint);
+	}
+	
+	/*
+	 * 退出日志
+	 */
+	@Before("execution(* com.imooc.sm.controller.SelfController.logout(..))")
+	public void logoutLog(JoinPoint joinPoint) {
+		log(joinPoint);
+	}
+	
+	
+	// 可复用方法
+	private void log(JoinPoint joinPoint) {
+		Log log = new Log();
+		log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
+		log.setOperation(joinPoint.getSignature().getName());
+		HttpServletRequest request = (HttpServletRequest) joinPoint.getArgs()[0];
+		HttpSession session = request.getSession();
+		Object obj = session.getAttribute("USER");
+		if(obj != null) {
+			Staff staff = (Staff) obj;
+			log.setOperator(staff.getAccount());
+			log.setResult("正常");
+		}
+		logService.addLoginLog(log);
 	}
 	
 	
